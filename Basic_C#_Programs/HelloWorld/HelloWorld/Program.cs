bb5c50da673806d096c62645b5d9b484b6446444
Garrett <wicketspaw@gmail.com>
2025-10-28 17:42:47 -0700

    First few small assignments to push

---
+++ Basic_C#_Programs/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+class Program
+{
+      static void Main()
+      {
+           Console.WriteLine("Hello, World!");
+           Console.ReadLine();
+
+           string escapeSequence = "This is a line with a tab:\tEnd of the line.";
+           Console.WriteLine(escapeSequence);
+    }
+}
+
