f8615c4b2739e50ad726ac10d55eff050286d6c2
Garrett <wicketspaw@gmail.com>
2025-10-31 20:30:41 -0700

    additional files from math ops

---
+++ Basic_C#_Programs/MathOperators/MathOperators/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 = 10;
+            //int num2 = 6;
+            //int addEquate = num1 + num2;
+            //Console.WriteLine(addEquate);
+
+            //int num3 = 25;
+            //int num4 = 8;
+            //int subbEquate = num3 - num4;
+            //Console.WriteLine(subbEquate);
+
+            //int num5 = 9;
+            //int num6 = 9;
+            //int multEquate = num5 * num6;
+            //Console.WriteLine(multEquate);
+
+            //int num7 = 81;
+            //int num8 = 9;
+            //int divEquate = num7 / num8;
+            //Console.WriteLine(divEquate);
+
+            //string fName = "Garrett ";
+            //string lName = "Fisher";
+            //string fullName = fName + lName;
+            //Console.WriteLine(fullName);
+
+            //int total = 5 + 10;
+            //int otherTotal = 12 + 22;
+            //int combined = total + otherTotal;
+            //Console.WriteLine(combined);
+            //Console.ReadLine();
+
+            //int difference = 10 - 5;
+            //Console.WriteLine("Ten minus Five equals " + difference.ToString());
+            //Console.ReadLine();
+
+            //int product = 10 * 5;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //double quotient = 100.0 / 17.0;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+            //int remainder = 11 % 2;
+            //Console.WriteLine(remainder);
+            //Console.ReadLine();
+
+            //int num1 = 200 / 3;
+            //Console.WriteLine(num1);
+
+            //bool trueOrFalse = 12 < 5;
+            //Console.WriteLine(trueOrFalse.ToString());
+            //Console.ReadLine();
+
+            int roomTemp = 70;
+            int currentTemp = 68;
+            bool isWarm = currentTemp != roomTemp;
+            Console.WriteLine(isWarm);
+            Console.ReadLine();
+        }
+    }
+}
