74e489e218383bd36ac726f093fb29b93552353a
Garrett <wicketspaw@gmail.com>
2025-10-31 20:28:31 -0700

    Number calculator assignment

---
+++ Basic_C#_Programs/NumberCruncher/NumberCruncher/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberCruncher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z, c;
+            double a;
+            bool b;
+            char operation;
+
+            //User input "x"
+            Console.Write("Enter first number (x): ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            //User input for operation to perform (*, +, /, >, %)
+            Console.Write("Enter operation (*, +, /, >, %): ");
+            operation = Convert.ToChar(Console.ReadLine());
+
+            //Variables for (y, z, a, b, c)
+            y = 50;
+            z = 25;
+            a = 12.5;
+            b = x > 50;
+            c = 7;
+
+
+            //Conditional statements for operation
+                if (operation == '*')
+                {
+                    Console.WriteLine("Result: " + (x * y));
+                }
+                else if (operation == '+')
+                {
+                    Console.WriteLine("Result: " + (x + x));
+                }
+                else if (operation == '/')
+                {
+                    Console.WriteLine("Result: " + (x / a));
+                }
+                else if (operation == '>')
+                {
+                    Console.WriteLine("Result: " + b);
+                }
+                else if (operation == '%')
+                {
+                    Console.WriteLine("Result: " + (x % c));
+                }
+                else
+                { Console.WriteLine("Invalid operation."); }
+        }
+    }
+}
---
+++ Basic_C#_Programs/NumberCruncher/NumberCruncher/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NumberCruncher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NumberCruncher")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6411b107-5b2c-4042-9c4d-93280290eb9a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
