cf5130b071cf9a6041b314f9ad00b788af5267c1
Garrett <wicketspaw@gmail.com>
2025-11-03 07:50:18 -0800

    Package shipping quote program per course assignment

---
+++ Basic_C#_Programs/PackageShippingQuote/PackageShippingQuote/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackageShippingQuote
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instructions below to get a shipping quote.");
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter the package weight:");
+            int weight = Convert.ToInt32(Console.ReadLine());
+
+            if (weight >= 51)
+            {
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
+                Console.ReadLine();
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Thank you! Please continue to enter the dimensions");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("Please enter the package width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package height:");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the package length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            int dimensions = width + height + length;
+
+            if (dimensions <= 50)
+            {
+                double quote = (width * height * length * weight) / 100.00;
+                Console.WriteLine("Your esitmated total for shipping this package is: $" + quote);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Package is too big to be shipped via Package Express.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
