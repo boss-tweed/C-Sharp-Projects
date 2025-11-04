e764062a1a8ae3a43162a6d76e7c775a61a3d2db
Garrett <wicketspaw@gmail.com>
2025-11-03 19:33:20 -0800

    Modified program per course assignment grading

--- Basic_C#_Programs/PackageShippingQuote/PackageShippingQuote/Program.cs
+++ Basic_C#_Programs/PackageShippingQuote/PackageShippingQuote/Program.cs
@@ -11,23 +11,23 @@ namespace PackageShippingQuote
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Package Express. Please follow the instructions below to get a shipping quote.");
-            Console.ReadLine();
 
+            //User input for package weight
             Console.WriteLine("Please enter the package weight:");
             int weight = Convert.ToInt32(Console.ReadLine());
 
+            //If else statement to check weight limit
             if (weight >= 51)
             {
-                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
-                Console.ReadLine();
+                Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");                
                 return;
             }
             else
             {
-                Console.WriteLine("Thank you! Please continue to enter the dimensions");
-                Console.ReadLine();
+                Console.WriteLine("Thank you! Please continue to enter the dimensions");              
             }
 
+            //User input for package dimensions
             Console.WriteLine("Please enter the package width:");
             int width = Convert.ToInt32(Console.ReadLine());
 
@@ -37,18 +37,18 @@ namespace PackageShippingQuote
             Console.WriteLine("Please enter the package length:");
             int length = Convert.ToInt32(Console.ReadLine());
 
+            //Calculate total dimensions
             int dimensions = width + height + length;
 
+            //If else statement to check dimension limit and calculate quote
             if (dimensions <= 50)
             {
                 double quote = (width * height * length * weight) / 100.00;
-                Console.WriteLine("Your esitmated total for shipping this package is: $" + quote);
-                Console.ReadLine();
+                Console.WriteLine("Your esitmated total for shipping this package is: $" + quote);                
             }
             else
             {
-                Console.WriteLine("Package is too big to be shipped via Package Express.");
-                Console.ReadLine();
+                Console.WriteLine("Package is too big to be shipped via Package Express.");                
             }
         }
     }
