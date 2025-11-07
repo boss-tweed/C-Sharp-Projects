ec09a3def628cbb8b2fcae338cae83b4f1e24276
Garrett <wicketspaw@gmail.com>
2025-11-07 07:13:39 -0800

    Updated program w/ a while loop so as to continue program if exception handling occured up to and untill successful math formula executed

--- Basic_C#_Programs/TryCatchAssignment/TryCatchAssignment/Program.cs
+++ Basic_C#_Programs/TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -10,38 +10,47 @@ namespace TryCatchAssignment
     {
         static void Main(string[] args)
         {
-            //try/catch block
-            try
+            bool succeeded = false;
+
+            while (!succeeded)
             {
-                //List of integers where user inputs a number that divides each integer
-                //in the list and outputs to the console
-                List<int> toDivideBy = new List<int>() { 84, 66, 52, 98, 107, 121, 243 };
-
-                Console.WriteLine("Please enter a number for which list of intergers will divide by");
-                int divisor = Convert.ToInt32(Console.ReadLine());
-
-                for (int i = 0; i < toDivideBy.Count; i++)
+                //try/catch block
+                try
                 {
-                    int result = (int)toDivideBy[i] / divisor;
-                    Console.WriteLine($"{toDivideBy[i]} / {divisor} = {result}");
+                    //List of integers where user inputs a number that divides each integer
+                    //in the list and outputs to the console
+                    List<int> toDivideBy = new List<int>() { 84, 66, 52, 98, 107, 121, 243 };
+
+                    Console.WriteLine("Please enter a number for which list of intergers will divide by");
+                    int divisor = Convert.ToInt32(Console.ReadLine());
+
+                    for (int i = 0; i < toDivideBy.Count; i++)
+                    {
+                        int result = (int)toDivideBy[i] / divisor;
+                        Console.WriteLine($"{toDivideBy[i]} / {divisor} = {result}");
+                    }
+                    //So loop can exit
+                    succeeded = true;   
                 }
-                Console.WriteLine("Press enter to exit...");                
+
+                //Exception handling of the try catch block displaying error messages when
+                //incorrect inputs are used
+                catch (FormatException)
+                {
+                    Console.WriteLine("Must enter whole numbers only...");
+                }
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
-            //Exception handling of the try catch block displaying error messages when
-            //incorrect inputs are used
-            catch (FormatException)
-            {
-                Console.WriteLine("Must enter whole numbers only...");
-                return;
-            }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            finally
-            {
-                Console.ReadLine();
-            }                 
+            Console.WriteLine("Program has succesfully executed after exception handling");
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
         }
     }
 }
