b9425082e4e8012f064d442a2b22827e7b450142
Garrett <wicketspaw@gmail.com>
2025-11-07 06:57:01 -0800

    Updated program to include while loop so program continues after try/catch block executes exception handling. Program does not exit till successful math formula executes

---
+++ Basic_C#_Programs/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool succeeded = false;
+
+        while (!succeeded)
+        {
+            try
+            {
+                Console.WriteLine("Pick a number");
+                int numberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick a second number");
+                int numberTwo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Dividing the two...");
+                int numberThree = numberOne / numberTwo;
+                Console.WriteLine(numberOne + " divided by " + numberTwo + " = " + numberThree);
+
+                //So loop can exit
+                succeeded = true;
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Please use any whole number other than 0");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        //Message to user that exception handling was successful aand program continued
+        Console.WriteLine("Execution has continued after exception handling");
+        Console.WriteLine("Press Enter to exit...");
+        Console.ReadLine();       
+    }
+}
