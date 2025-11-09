9d05134e1ecf41ab6963eecb384cbf366a8f6a32
Garrett <wicketspaw@gmail.com>
2025-11-08 17:59:53 -0800

    Methods submission per course assignment

---
+++ Basic_C#_Programs/MethodsAssignment/MethodsAssignment/Integer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    public class Integer
+    {
+        ///<summary>
+        ///Returns the input divided by 3
+        ///</summary>  
+        public static int  Divide(int value) 
+        {
+            return value / 3;
+        }
+
+        ///<summary>
+        ///Returns input multiplied by 36.5
+        ///</summary>
+        public static double Times(double value)
+        {
+            return value * 36.5;
+        }
+
+        ///<summary>
+        ///Returns input subtracted by 20
+        ///</summary>
+        public static int Minus(int value)
+        {
+            return value - 20;
+        }
+
+    }
+}
---
+++ Basic_C#_Programs/MethodsAssignment/MethodsAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {    
+            //Asks for user input to do math operations
+            Console.WriteLine("Please select a number with which to perform the math operations on");
+            int input = Convert.ToInt32(Console.ReadLine());
+            
+            
+            //Call each method in Integer class and display result
+            int divided = Integer.Divide(input);
+            double times = Integer.Times(input);
+            int minus = Integer.Minus(input);
+
+            Console.WriteLine("Input: " + input);
+            Console.WriteLine("Divide (value / 3): " + divided);
+            Console.WriteLine("Times (value * 36.5): " + times);
+            Console.WriteLine("Minus (value - 20): " + minus);
+
+            Console.WriteLine("Press Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
