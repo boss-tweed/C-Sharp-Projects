6edbbb0af23398af51fa6422a91ab75f3eebd075
Garrett <wicketspaw@gmail.com>
2025-11-08 19:43:44 -0800

    Main Method submission per course assignment

---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the ThreeMethods class, call MethodOne and display to console
+            ThreeMethods mathOps = new ThreeMethods();
+            int number1 = mathOps.MethodOne(25);
+
+            Console.WriteLine("MethodOne (input + 10): " + number1);
+
+            //Instantiate ThreeMethods class to call overloaded MethodOne and display to console
+            ThreeMethods mathOps2 = new ThreeMethods();
+            int number2 = mathOps2.MethodOne(67.75m);
+
+            Console.WriteLine("MethodOne (input * 20): " + number2);
+
+            //Instantiate ThreeMethods class to call 3rd overloaded MethodOne and display to console
+            ThreeMethods mathOps3 = new ThreeMethods();
+            int number3 = mathOps3.MethodOne("175");
+
+            Console.WriteLine("MethodOne (input - 75): " + number3);
+        }
+    }
+}
---
+++ Basic_C#_Programs/MainMethodAssignment/MainMethodAssignment/ThreeMethods.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class ThreeMethods
+    {
+        ///<summary>
+        ///Takes integer, creates a math op and returns answer as an integer
+        ///</summary>
+        public int MethodOne(int value)
+        {
+            return value + 10;
+        }
+
+        ///<summary>
+        ///2nd mthod of the same name takes decimal, creates a 
+        ///different math op and returns answer as an integer
+        ///</summary>
+        public int MethodOne(decimal value)
+        {
+            return Convert.ToInt32(value * 20);
+        }
+
+        ///<summary>
+        ///3rd method of same name takes string, converts to integer, 
+        ///does math op and returns an integer
+        ///</summary>
+        public int MethodOne(string value)
+        {
+            int stringToInt = Convert.ToInt32(value);
+            return stringToInt - 75;
+        }
+    }
+}
