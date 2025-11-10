e8d985ef7d1f5c8fdb490893a5b1b38c24d46a93
Garrett <wicketspaw@gmail.com>
2025-11-09 20:28:39 -0800

    Class submission per course assignment

---
+++ Basic_C#_Programs/ClassAssignment/ClassAssignment/DivideMethod.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    internal static class DivideMethod
+    {
+        public static void OutputInt(int value)
+        {
+            //Create a void method that outputs an integer and divide
+            //the data passed to it by 2
+            int sum = value / 2;
+
+            Console.WriteLine("The output of the called method is: " + sum);
+        }
+
+        //Create a method w/ output parameters
+        public static int OutputInt2(int value, out int result)
+        {
+            result = value * 45;
+            return result;
+        }
+
+        public static double OutputInt2(double value)
+        {
+            double result = value * 13.75;
+            return result;
+
+        }
+    }
+}
---
+++ Basic_C#_Programs/ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This class was instantiated to call the methods in DivideMethod class.
+            //But later on per assignment, I made the DivideMethod class static so instantiation is not necessary
+
+            //User enters a number to call the DivideMethod on it
+            Console.WriteLine("Please enter a number the method will divide with:");
+            string intOut = Console.ReadLine();
+
+            if (int.TryParse(intOut, out int val))
+            {
+                DivideMethod.OutputInt(val);
+            }
+
+            //User input to call the 2nd method w/ output parameter
+            Console.WriteLine("Enter another number to call the second method with output parameter:");
+            int intOut2 = Convert.ToInt32(Console.ReadLine());
+
+            int result;
+            DivideMethod.OutputInt2(intOut2, out result);
+            Console.WriteLine("The output of the second method is: " + result);
+
+            //User input to call the overloaded method
+            Console.WriteLine("Enter a decimal number to call the overloaded method:");
+            double doubleOut = Convert.ToDouble(Console.ReadLine());
+
+            double overloadResult = DivideMethod.OutputInt2(doubleOut);
+            Console.WriteLine("The output of the overloaded method is: " + overloadResult);
+        }   
+    }
+}
