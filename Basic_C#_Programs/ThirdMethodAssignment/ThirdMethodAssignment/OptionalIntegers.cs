de9f9f3c148f488a57679be21fbf57cefcbe80d9
Garrett <wicketspaw@gmail.com>
2025-11-09 16:22:49 -0800

    3rd method submission per course assignment

---
+++ Basic_C#_Programs/ThirdMethodAssignment/ThirdMethodAssignment/OptionalIntegers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdMethodAssignment
+{
+    internal class OptionalIntegers
+    {
+        ///<summary>
+        ///Method that takes two integers as parameters, one optional, then does math op and returns
+        ///result as integer
+        ///</summary>
+        public int OptionalIntMethod(int val1, int val2 = 0)
+        {
+            int result = val1 + val2;
+            return result;
+        }
+    }
+}
---
+++ Basic_C#_Programs/ThirdMethodAssignment/ThirdMethodAssignment/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThirdMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate the class. Ask the user to input 2 numbers, one can be optional.
+            //Call the method in the class, passing in the numbers, then display the result to the user.
+            OptionalIntegers optionalIntegers = new OptionalIntegers();
+
+            int numInput1;
+           
+            Console.WriteLine("Please enter the first number");
+            if (int.TryParse(Console.ReadLine(), out numInput1))
+                   
+            Console.WriteLine("Please enter the second number (or press Enter to skip)");
+            string secondLine = Console.ReadLine();
+
+            int result;
+            if (string.IsNullOrWhiteSpace(secondLine))
+            {
+                //Call method w/ only one paremter
+                result = optionalIntegers.OptionalIntMethod(numInput1);
+            }
+            else if (int.TryParse(secondLine, out int numInput2))
+            {
+                //Call method w/ both parameters
+                result = optionalIntegers.OptionalIntMethod(numInput1, numInput2);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input for 2nd number. Using 0 as default");
+                result = optionalIntegers.OptionalIntMethod(numInput1);
+            }
+
+            Console.WriteLine($"The result is: {result}");
+        }
+    }
+}
