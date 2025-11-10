51a75c9c8812f52da8fdc9ce091d8546f06b5dc0
Garrett <wicketspaw@gmail.com>
2025-11-10 08:15:55 -0800

    Methods and objects submission per course assignment

---
+++ Basic_C#_Programs/MethodsAndObjects/MethodsAndObjects/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    public class Person
+    {
+        // Create properties for FirstName and LastName
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public void SayName()
+        {
+            //Create a method called SayName that prints the person's full name to the console
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodsAndObjects/MethodsAndObjects/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a new Employee object
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            //Call the SayName method from the Person class
+            employee.SayName();
+        }
+    }
+}
