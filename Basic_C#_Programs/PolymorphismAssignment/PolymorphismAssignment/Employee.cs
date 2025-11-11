a2557e5f8485c2558ee0185fe362b760a0347961
Garrett <wicketspaw@gmail.com>
2025-11-11 13:33:58 -0800

    Interface example and submission per course assignment

---
+++ Basic_C#_Programs/PolymorphismAssignment/PolymorphismAssignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+     public class Employee : IQuittable
+    {
+        //Inheriting the interface method and implementing it
+        public void Quit()
+        {
+            Console.WriteLine("Employee has quit.");
+        }
+    }
+}
---
+++ Basic_C#_Programs/PolymorphismAssignment/PolymorphismAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create an object type of IQuittable
+            IQuittable quittable = new Employee();
+
+            //Interface reference to the object. Call the Quit() method on the object
+            IQuittable emp = quittable;
+            emp.Quit();
+
+        }
+    }
+}
