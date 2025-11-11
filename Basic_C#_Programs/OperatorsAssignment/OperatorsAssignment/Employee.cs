5edf5e78db16e26866ea604841f3e8761f51903a
Garrett <wicketspaw@gmail.com>
2025-11-11 15:03:32 -0800

    Operators submission per course assignment

---
+++ Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    public class Employee
+    {
+        //Create 3 properties: ID, FirstName, LastName
+        public int ID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        //Overload the == operator to compare two Employee objects.
+        public static bool operator ==(Employee emp1, Employee emp2)
+        {
+            if (ReferenceEquals(emp1, emp2)) return true;
+            if (ReferenceEquals(emp1, null) || ReferenceEquals(emp2, null)) return false;
+            return emp1.ID == emp2.ID;
+        }
+
+        public static bool operator !=(Employee emp1, Employee emp2)
+        {
+            return !(emp1.ID == emp2.ID);
+        }
+    }
+}
---
+++ Basic_C#_Programs/OperatorsAssignment/OperatorsAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiat two Employee objects
+            Employee emp1 = new Employee() { ID = 1, FirstName = "Garrett", LastName = "Fisher" };
+            Employee emp2 = new Employee() { ID = 2, FirstName = "Indica", LastName = "Marie" };
+
+            //compare the two objects using the overloaded == operator and display the result
+            bool areEqual = emp1 == emp2;
+            bool areNotEqual = emp1 != emp2;
+
+            Console.WriteLine($"Are emp1 and emp2 equal? {areEqual}");
+            Console.WriteLine($"Are emp1 and emp2 not equal? {areNotEqual}");
+        }
+    }
+}
