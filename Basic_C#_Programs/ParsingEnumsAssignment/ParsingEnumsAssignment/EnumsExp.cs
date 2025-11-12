23a13b552a4ee51ec72b8a941386d6c6d9620944
Garrett <wicketspaw@gmail.com>
2025-11-12 09:38:32 -0800

    Parsing enums submission per course assignment

---
+++ Basic_C#_Programs/ParsingEnumsAssignment/ParsingEnumsAssignment/EnumsExp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    public class EnumsExp
+    {
+        //Enum declaration
+        public enum DaysOfWeek
+        {
+            Sunday = 1,
+            Monday = 2,
+            Tuesday = 3,
+            Wednesday = 4,
+            Thursday = 5,
+            Friday = 6,
+            Saturday = 7
+        }
+    }
+}
---
+++ Basic_C#_Programs/ParsingEnumsAssignment/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //instantiate class
+            EnumsExp example = new EnumsExp();
+
+            //Try/catch block to handle exceptions
+            bool success = false;
+            while (!success)
+            {
+                try
+                {
+                    //Prompt user to enter current a day of the week
+                    Console.WriteLine("Please enter the current day of the week:");
+
+                    //Get user input and parsae enum
+                    string dayInput = Console.ReadLine();
+
+                    //Parse input of enum using TryParse
+                    EnumsExp.DaysOfWeek currentDay;
+                    if (Enum.TryParse<EnumsExp.DaysOfWeek>(dayInput, true, out currentDay)
+                        && Enum.IsDefined(typeof(EnumsExp.DaysOfWeek), currentDay))
+                    {
+                        Console.WriteLine($"You have entered: {currentDay} ({(int)currentDay})");
+                        success = true;
+                    }
+                    else
+                    {
+                        throw new ArgumentException();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Please enter an actual day of the week"); ;
+                }
+            }
+        }
+    }
+}
