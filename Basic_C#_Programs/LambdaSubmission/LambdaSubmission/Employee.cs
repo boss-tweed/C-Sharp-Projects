c3300b1345dd0dc39c1288a03313c6f35b01b29f
Garrett <wicketspaw@gmail.com>
2025-11-13 09:28:18 -0800

    Lambda submission per course assignment

---
+++ Basic_C#_Programs/LambdaSubmission/LambdaSubmission/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaSubmission
+{
+    public class Employee
+    {
+        //ID, FirstName, LastName properties
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/LambdaSubmission/LambdaSubmission/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+
+            //Create a list of at least 10 employees.
+            List<string> employees  = new List<string>() { "Joe James", "Joe Jack", "Garrett Fisher", "Delaney Rose", "Jordan Glen", "Shannon Kate", "Gary Daniel", "Sandra Jean", "Jim Jacobs", "Janet Fina" };
+
+            //foreach loop creates a new list of all employees w/ the first name Joe.
+            List<string> joeEmployees = new List<string>();
+            foreach (string emp in employees) // Renamed loop variable to 'emp' to avoid CS0136
+            {
+                if (string.Equals(emp, "Joe", StringComparison.OrdinalIgnoreCase))
+                {
+                    joeEmployees.Add(emp);
+                }
+            }
+
+            //Perform the same action w/ a Lambda expression
+            List<string> joEmployees = employees
+                .Where(e => string.Equals(e, "Joe", StringComparison.OrdinalIgnoreCase))
+                .ToList();
+
+            //Lambda expression w/ list of employees w/
+            //an ID number greater than 5
+            List<string> idEmployees = employees
+                .Where((e, index) => index > 5)
+                .ToList();
+        }
+    }
+}
