eb04de8b3defaaf4d9d1ff3cd87d957db21ba03f
Garrett <wicketspaw@gmail.com>
2025-11-27 19:49:28 -0800

    Student database console app for course assignment

---
+++ Basic_C#_Programs/StudentDbSubmission/StudentDbSubmission/Grade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDbSubmission
+{
+    public class Grade
+    {
+        public int GradeId { get; set; }
+        public string GradeName { get; set; }
+        public string GradeDescription { get; set; }
+
+        public ICollection<Student> Students { get; set; }
+    }
+}
